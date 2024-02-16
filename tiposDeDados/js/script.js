0e9e62a747717807ebb1f747a07a623987e9c1b6
bergdantas <bergdantas@msn.com>
2024-02-16 10:56:29 -0300

    tipos de dados no js

---
+++ tiposDeDados/js/script.js
@@ -0,0 +1,30 @@
+//declaração de variáveis
+var varString ='Cheguei atrasado!';
+var varNumeric = 3.875;
+var varBoolean = false;
+var varObject = ['Muito obrigado' , 'Álvaro Dias'];
+var varMatriz = [[1, 3],[5, 6],[5, 6]];
+var varDate = new Date(2024,1,16);
+var varFunction = soma(3,4);
+
+//a funçaõ também é um tipo de dado
+function soma(a,b){
+    return a+b;
+}
+
+//o objeto pessoa também é um tipo de dado
+var pessoa = {
+    nome: "berg",
+    profissao: "Professor"
+}
+
+//checando tipos:
+console.log(typeof varString);
+console.log(typeof varNumeric);
+console.log(typeof varBoolean);
+console.log(typeof varObject);
+console.log(typeof varMatriz);
+console.log(typeof varDate);
+console.log(typeof varFunction);
+console.log(typeof soma);
+console.log(typeof pessoa);
