a9bfad523b8c8629bbdc07d01d33a5a407ece838
bergdantas <54481994+bergdantas@users.noreply.github.com>
2024-02-28 11:20:25 -0300

    estrutura de repetição: while

---
+++ while/js/script.js
@@ -0,0 +1,19 @@
+/*
+*Como manipulei o DOM, seque o material complementar: https://www.w3schools.com/jsref/dom_obj_document.asp 
+*/
+//função principal
+function calcula(){
+    //faz a captura de um valor do input do html e faz o casting
+    var vI = Number(document.getElementById("vI").value);
+    var vF = Number(document.getElementById("vF").value);
+    var cont=vI, acum=0;
+    while(cont<=vF){
+        //atualizo o acumulador
+        acum=acum+cont;
+        //atualizo o contador
+        cont++;
+    }
+    
+    //faz o envio do resultado do resultado da função calcula() para o campo do HTML de id resultado
+    document.getElementById("resultado").innerHTML = "O valor do somatório é "+acum;
+}
