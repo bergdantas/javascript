5c077cbbe1aa40f9711c26a08f8efbeb958a1697
bergdantas <54481994+bergdantas@users.noreply.github.com>
2024-03-03 23:23:23 -0300

    entrada de dados

---
+++ particularidades/entrada/entrada.js
@@ -0,0 +1,7 @@
+function entrada(){
+    var nome = prompt("Digite seu nome");
+    var numeroDeEntrada = document.getElementById("numeroDeEntrada").value;
+
+    //faz a saída com as informações de entrada
+    document.write("Oi, "+nome+" você digitou "+numeroDeEntrada);
+}   
