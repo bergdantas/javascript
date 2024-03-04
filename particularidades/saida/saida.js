4292a17d9c5cedfe2f9707766d4abc9cc80dc642
bergdantas <54481994+bergdantas@users.noreply.github.com>
2024-03-03 23:05:24 -0300

    saída de dados

---
+++ particularidades/saida/saida.js
@@ -0,0 +1,9 @@
+//ide e navegador
+console.log("mensagem do console.log()");
+
+//navegador
+document.write("mensagem do document.write()");
+alert("mensagem do alert");
+
+//DOM
+document.getElementById("saida").innerHTML = "mensagem do DOM";
